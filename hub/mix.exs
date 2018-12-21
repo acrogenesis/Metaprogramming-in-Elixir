e07d7d018adae84a393ca901feff9b671dba7a34
Adrian Rangel <adrian.rangel@gmail.com>
2018-12-21 17:11:47 -0600

    not working ibrowser

--- hub/mix.exs
+++ hub/mix.exs
@@ -21,8 +21,9 @@ defmodule Hub.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ibrowser, github: "cmullaparthi/ibrowse", tag: "v4.4.1"},
+      {:poison, "~> 1.3.0"},
+      {:httpotion, "~> 1.0.0"}
     ]
   end
 end
